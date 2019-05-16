54fc1e1f0f28fda70816bc53bfb5ea79fa151872
dashwoood <parkhomenko.alya@gmail.com>
2019-05-16 14:21:20 +0200

    l

---
+++ MyProject/Assets/Scripts/Starship.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Transform.position example.
+//
+// Animate a sphere around a circle and bounce up and down.
+
+public class ExampleScript : MonoBehaviour
+{
+    private Vector3 spherePosition;
+    private float xzPosition, yPosition;
+    private float increaseXZPosition, increaseYPosition;
+
+    void Awake()
+    {
+        // Three seconds per circular rotation.
+        increaseXZPosition = (2.0f * Mathf.PI) / 3.0f;
+
+        // Random speed up/down.
+        increaseYPosition = (2.0f * Mathf.PI) / 1.3f;
+
+    }
+
+    void Update()
+    {
+        // Move sphere around the circle.
+        spherePosition = new Vector3(2.0f * Mathf.Sin(xzPosition), 4.0f * Mathf.Sin(yPosition), 2.0f * Mathf.Cos(xzPosition));
+        transform.position = spherePosition;
+
+        // Update the rotating position.
+        xzPosition += increaseXZPosition * Time.deltaTime;
+        if (xzPosition > 2.0f * Mathf.PI)
+        {
+            xzPosition = xzPosition - 2.0f * Mathf.PI;
+        }
+
+        // Update the up/down position.
+        yPosition += increaseYPosition * Time.deltaTime;
+        if (yPosition > Mathf.PI)
+        {
+            yPosition = yPosition - Mathf.PI;
+        }
+    }
+
+    // Display the changing position of the sphere.
+    void OnGUI()
+    {
+        GUIStyle fontSize = new GUIStyle(GUI.skin.GetStyle("label"));
+        fontSize.fontSize = 24;
+        GUI.Label(new Rect(20, 20, 300, 50), "Position: " + spherePosition.ToString("F2"), fontSize);
+    }
+}
---
+++ MyProject/Assets/Scripts/TextTimer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextTimer : MonoBehaviour
+{
+    public float time = 3; 
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}
