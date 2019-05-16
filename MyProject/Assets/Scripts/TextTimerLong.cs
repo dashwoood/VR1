089605fec9299c56bbc3df6e36d3c07a28a06f87
dashwoood <parkhomenko.alya@gmail.com>
2019-05-16 14:58:42 +0200

    vr

---
+++ MyProject/Assets/Scripts/TextTimerLong.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextTimerLong : MonoBehaviour
+{
+    public float time = 15;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}
